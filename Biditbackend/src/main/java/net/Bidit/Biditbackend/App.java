93629bf462dce80e8ff4e0248ebe4c9423e30639
Ashutosh Kanojiya <ashutoshkanojiya1@gmail.com>
2018-02-24 13:28:23 +0530

    My project setup completed successfully.

---
+++ Biditbackend/src/main/java/net/Bidit/Biditbackend/App.java
@@ -0,0 +1,13 @@
+package net.Bidit.Biditbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
